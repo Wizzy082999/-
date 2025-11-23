1dddc2f456cbb8d8ef1e5c6b17aa81831b8b47db
Wizzy082999 <tikuzasetuha01867@gmail.com>
2025-11-23 19:11:10 +0800

    types.ts

--- types.ts
+++ types.ts
@@ -1,7 +1,7 @@
 
 export type WeatherType = 'snow' | 'rain' | 'sunny' | 'starry';
 
-export type DecorationType = 'santa' | 'tree' | 'sock' | 'cat' | 'dog' | 'star' | 'gift' | 'rainbow' | 'flower' | 'moon' | 'snowman' | 'bell' | 'lantern' | 'firecracker' | 'umbrella' | 'milk';
+export type DecorationType = 'santa' | 'tree' | 'sock' | 'cat' | 'dog' | 'star' | 'gift' | 'rainbow' | 'flower' | 'moon' | 'snowman' | 'bell' | 'lantern' | 'firecracker' | 'umbrella' | 'milk' | 'flash' | 'beatheart'| 'popcorn'|'film';
 
 export interface Decoration {
   id: string;
